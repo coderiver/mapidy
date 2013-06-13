db4266fe01eb4bf71395463b72b35c41ec610e39
dpismenny <denis.pismenny@gmail.com>
2013-06-13 16:13:30 +0300

    people pages

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-	// ---------- Popup for contact information -------------------- //
+	// ---------- Popup for create dropdown -------------------- //
 		$('.js-drop').click(function() {
 		 if ($(this).hasClass('is-open')) {
 		  $(this).removeClass('is-open');
@@ -22,5 +22,33 @@ $(document).ready(function() {
 		    event.stopPropagation();
 		});
 
+	// ---------- Popup for people list page -------------------- //
+		$('.js-invite').click(function() {
+		 if ($(this).hasClass('is-open')) {
+		  $(this).removeClass('is-open');
+		  $(this).parent().children('.js-invite-drop').fadeOut();
+		 }
+		 else {
+		  $(this).addClass('is-open');
+		  $(this).parent().children('.js-invite-drop').fadeIn();
+		 }
+		 return false;
+		});
+
+		$(document).click(function() {
+		 $('.js-invite').removeClass('is-open');
+		 $('.js-invite-drop').fadeOut();
+		});
+		$('.js-invite-drop').click(function(event){
+		    event.stopPropagation();
+		});
+
+	// ---------- Popup with overlay for people list page -------------------- //
+		$('body').click(function(){
+			$('.overlay,.popup').toggleClass('is-shown');
+		});
+
+	// ----------------  Chosen plagin -------------------- //
+		$(".chzn-select").chosen({disable_search_threshold: 100});
 
 });
